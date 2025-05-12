3d86dc9f4d51451076c98b53461256a36ddee257
Hakim  Al-Abbasi <89461089+HakimDeve@users.noreply.github.com>
2025-05-12 18:25:19 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW___LP
+{
+    internal class Program
+    {
+         
+        static void Main(string[] args)
+        {
+            Console.Write("Haim Al-Abbasi\n");
+            Console.WriteLine("This is My First Program");
+            int Number = 10 + 5;
+            Console.WriteLine("The Result of 10 + 5 = "+ Number);
+            
+            Console.ReadKey();
+
+        }
+    }
+}
